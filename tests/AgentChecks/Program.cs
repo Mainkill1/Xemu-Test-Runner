98005dce4698e058df91f52abb31c9cedd23e542
Mainkill1 <Mainkill1@gmail.com>
2026-09-24 00:54:30 -0700

    test: run disk asset schema compatibility checks

--- tests/AgentChecks/Program.cs
+++ tests/AgentChecks/Program.cs
@@ -6,6 +6,7 @@ if (args.Contains("--client", StringComparer.Ordinal))
 }
 else
 {
+    DiskAssetSchemaChecks.Register(checks);
     DiskAssetApiChecks.Register(checks);
     ObservationChecks.Register(checks);
     TemplateChecks.Register(checks);
